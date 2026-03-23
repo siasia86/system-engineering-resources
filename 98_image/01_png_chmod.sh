f5d543f364ed1056ae9989c80df142d44824b5c1
siasia86 <siasia.linux@gmail.com>
2026-03-23 18:14:17 +0900

    2026-03-23-18:14:17  resource-utilization-monitoring

--- 98_image/01_png_chmod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-find /root/32_system-engineering-resources/98_image/ -type d -exec chmod 755 {} \;
-find /root/32_system-engineering-resources/98_image/ -type f -exec chmod 644 {} \;
-chown sjyun:sjyun /root/32_system-engineering-resources/ -R
