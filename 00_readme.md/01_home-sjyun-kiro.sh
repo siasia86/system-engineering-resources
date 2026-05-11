e6818a384009e1637b63863195cd052fb209452b
siasia86 <siasia.linux@gmail.com>
2026-05-11 14:35:22 +0900

    2026-05-11 14:35:22 skill

--- 00_readme.md/01_home-sjyun-kiro.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "rsync --dry-run"
-rsync -av /home/sjyun/.kiro/ /root/sj_del/00_default/.kiro/ --exclude .cli_bash_history --exclude sessions --exclude *.swp  -n
-sleep 5
-rsync -av /home/sjyun/.kiro/ /root/sj_del/00_default/.kiro/ --exclude .cli_bash_history --exclude sessions --exclude *.swp 
-
-
